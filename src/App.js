333548d9bc845983810d432ff7398b2b4a3e6279
eeeeee6 <89331363+eeeeee6@users.noreply.github.com>
2025-07-19 00:51:19 +0800

    fix: 還原美化CSS與登入註冊畫面

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import DiaryForm from "./DiaryForm";
-import DiaryList from "./DiaryList";
+import Auth from "./Auth";
 import './App.css';
 
 function App() {
   return (
     <div className="app-container" style={{ background: '#f5f6fa', minHeight: '100vh' }}>
-      <DiaryForm />
-      <hr />
-      <div className="diary-list-title">我的日記</div>
-      <DiaryList onlyMine={true} />
+      <Auth />
     </div>
   );
 }
